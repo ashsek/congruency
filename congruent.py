a25eabd6d98be3a85fee4e615020324e545fb013
Ashwin Sekhari <ashwinsekhari@gmail.com>
2018-01-20 14:49:49 +0530

    adding plot functionality

--- congruent.py
+++ congruent.py
@@ -9,12 +9,14 @@ keep a check on negative coordinates :/
 """
 import itertools 
 import helper as hl
+import matplotlib.pyplot as plt
 
 points = []
 triangles_all = []
 triangles_possible = []
 triangle_pairs = []
-        
+plot = []
+
 # it takes the input from the user as points and store them as coordinate obejcts 
 for i in range(int(input())):
     a,b = list(map(int,input().split()))
@@ -42,3 +44,15 @@ for m in triangle_pairs:
     
 print('Congurent Triangles are',hl.congurent)
 
+#[[1,1], [2,1], [2,2], [1,2]]
+for u in hl.congurent:
+    a0 = [[u[0].p1.x,u[0].p1.y],[u[0].p2.x,u[0].p2.y],[u[0].p3.x,u[0].p3.y]]
+    a1 = [[u[1].p1.x,u[1].p1.y],[u[1].p2.x,u[1].p2.y],[u[1].p3.x,u[1].p3.y]]
+    if a0 not in plot:
+        plot.append(a0)
+    if a1 not in plot:
+        plot.append(a1)
+    break
+
+print('-----------',plot)
+    
